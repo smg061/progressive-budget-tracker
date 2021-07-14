2cf78c36099532b06298c4adf23968acc5b58e40
smg061 <max.go.95@gmail.com>
2021-07-14 15:30:35 -0500

    added connection string to online mongodb

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,8 +9,11 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-
 ];
+
+
+
+
 
 // install
 self.addEventListener("install", function (evt) {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
